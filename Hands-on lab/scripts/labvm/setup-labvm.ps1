373cc6906f02d0169951381f9b703770bf1f8110
Daron Yondem <daron@yondem.com>
2021-02-21 12:15:00 +0300

    Lab VM Script Extension Improvements
    Removing chocolatey based SMSS install.
    Adding ZIP File Download Corruption Check
    Adding Stand-Alone SMSS Install

--- Hands-on lab/scripts/labvm/setup-labvm.ps1
+++ Hands-on lab/scripts/labvm/setup-labvm.ps1
@@ -1,19 +1,43 @@
 param($labFilesName="")
 
-# Install SSMS
+# Install Chocolatey
+# Previously, SMSS was being installed through chocolatey. 
+# It is now installed as a stand-alone install. Leaving chocolatey in case it can be used during the Lab.
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')) 
-choco install sql-server-management-studio -y
 
-# Download Lab Files
-$labFilesFolder = "C:\MCW"
-if ([string]::IsNullOrEmpty($labFilesName) -eq $false)
-{
-    Write-Host "Make sure folder exists"
-    if ((Test-Path $labFilesFolder) -eq $false)
+# We are waiting for other potential MSI installs to finish.
+function Wait-Install {
+    $msiRunning = 1
+    $msiMessage = ""
+    while($msiRunning -ne 0)
     {
-        New-Item -Path $labFilesFolder -ItemType directory
+        try
+        {
+            $Mutex = [System.Threading.Mutex]::OpenExisting("Global\_MSIExecute");
+            $Mutex.Dispose();
+            $DST = Get-Date
+            $msiMessage = "An installer is currently running. Please wait...$DST"
+            Write-Host $msiMessage 
+            $msiRunning = 1
+        }
+        catch
+        {
+            $msiRunning = 0
+        }
+        Start-Sleep -Seconds 1
     }
+}
 
-    Write-Host "Extract .ZIP file..."
-    Expand-Archive -Path $labFilesName -DestinationPath $labFilesFolder -Force
+Wait-Install
+(New-Object System.Net.WebClient).DownloadFile('https://aka.ms/ssmsfullsetup', 'C:\SSMS-Setup.exe')
+$pathArgs = {C:\SSMS-Setup.exe /Install /Quiet /Norestart /Logs logSSMS.txt}
+Invoke-Command -ScriptBlock $pathArgs
+
+# Download and extract the starter solution files
+# ZIP File sometimes gets corrupted
+New-Item -ItemType directory -Path C:\MCW
+while((Get-ChildItem -Directory C:\MCW | Measure-Object).Count -eq 0 )
+{
+    (New-Object System.Net.WebClient).DownloadFile($labFilesName, 'C:\MCW.zip')
+    Expand-Archive -LiteralPath 'C:\MCW.zip' -DestinationPath 'C:\MCW' -Force
 }
