b22bace46dc6b95595d01bfc53d1e975f8c1e406
Daron Yondem <daron@yondem.com>
2021-02-21 13:03:09 +0300

    Adding TLS 1.2 for Powershell VM Extension with Chocolatey

--- Hands-on lab/scripts/labvm/setup-labvm.ps1
+++ Hands-on lab/scripts/labvm/setup-labvm.ps1
@@ -1,5 +1,15 @@
 param($labFilesName="")
 
+# To resolve the error of https://github.com/microsoft/MCW-Modern-cloud-apps/issues/79
+# The cause of the error is that Powershell uses TLS 1.0 to connect to a website, but website security requires TLS 1.2.
+# You can change this behavior by running any of the below commands to use all protocols. You can also specify a single protocol.
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls, [Net.SecurityProtocolType]::Tls11, [Net.SecurityProtocolType]::Tls12, [Net.SecurityProtocolType]::Ssl3
+[Net.ServicePointManager]::SecurityProtocol = "Tls, Tls11, Tls12, Ssl3"
+
+# Chocolatey has been used for SMSS install. Now SMMS is being set up as a stand-alone install. 
+# Still leaving Chocolatey in case one of the labs uses it.
+iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')) 
+
 # We are waiting for other potential MSI installs to finish.
 function Wait-Install {
     $msiRunning = 1
