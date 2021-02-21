a756bc8d92491ddb33e4173964ab74045f867ab0
Daron Yondem <daron@yondem.com>
2021-02-21 13:20:47 +0300

    Disabling Internet Explorer Enhanced Security Configuration

--- Hands-on lab/scripts/labvm/setup-labvm.ps1
+++ Hands-on lab/scripts/labvm/setup-labvm.ps1
@@ -1,10 +1,22 @@
 param($labFilesName="")
 
+function Disable-InternetExplorerESC {
+    $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+    $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0 -Force
+    Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0 -Force
+    Stop-Process -Name Explorer -Force
+    Write-Host "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green
+}
+
 # To resolve the error of https://github.com/microsoft/MCW-Modern-cloud-apps/issues/79
 # The cause of the error is that Powershell uses TLS 1.0 to connect to a website, but website security requires TLS 1.2.
 # You can change this behavior by running any of the below commands to use all protocols. You can also specify a single protocol.
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls, [Net.SecurityProtocolType]::Tls11, [Net.SecurityProtocolType]::Tls12, [Net.SecurityProtocolType]::Ssl3
 [Net.ServicePointManager]::SecurityProtocol = "Tls, Tls11, Tls12, Ssl3"
+
+# Disable IE ESC
+Disable-InternetExplorerESC
 
 # Chocolatey has been used for SMSS install. Now SMMS is being set up as a stand-alone install. 
 # Still leaving Chocolatey in case one of the labs uses it.
